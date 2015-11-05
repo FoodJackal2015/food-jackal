d5b692d1fe1a3e93e895690a947ed5b69a01e739
Conor Thompson <conorthompson213@gmail.com>
2015-11-05 11:17:08 +0000

    Create general.php

---
+++ customer/general.php
@@ -0,0 +1,30 @@
+  
+    
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+        <br>
+        <br/>
+        <br>
+        </div>
+        <div class="row">
+
+            <div class="col-md-3">
+                <p class="lead">General</p>
+                <div class="list-group">
+                    <a href="settings.php?setting=My-Settings" class="list-group-item">My Settings</a>
+                    <a href="settings.php?setting=General" class="list-group-item active">General</a>
+                    <a href="settings.php?setting=Advanced" class="list-group-item">Advanced</a>
+                </div>
+            </div>
+
+            <div class="col-md-9">
+           
+
+            </div>
+
+        </div>
+
+    </div>
+    <!-- /.container -->
