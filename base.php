fce6bea94c78399fe47aca3083a76b1a578344fa
Conor Thompson <conorthompson213@gmail.com>
2015-11-12 20:02:12 +0000

    Create base.php

---
+++ base.php
@@ -0,0 +1,88 @@
+<br>
+<br>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Marketing Icons Section -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                    Welcome to FoodJackal
+                </h1>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4><i class="fa fa-fw fa-check"></i> Order food online</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Order and receive your food at any time within the IFSC district, perfect for any hungry NCI Student!</p>
+                        <a href="about.php" class="btn btn-default">Learn More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4><i class="fa fa-fw fa-gift"></i> Free &amp; Secure</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Absolute security on your personal details, and no added tax or costs on all of your orders.</p>
+                        <a href="about.php" class="btn btn-default">Learn More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4><i class="fa fa-fw fa-compass"></i> Easy to Use</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Standard user interface allows for smooth browsing and interaction.</p>
+                        <a href="about.php" class="btn btn-default">Learn More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        
+
+        <!-- Features Section -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="page-header">Based in the IFSC District</h2>
+            </div>
+            <div class="col-md-6">
+                <p>Serves all people of the district, students and employees, and passer-bys:</p>
+                <ul>
+                    <li><strong>Subway</strong>
+                    </li>
+                    <li>Centra</li>
+                    <li>Munchies</li>
+                    <li>Marks & Spencer</li>
+                    <li>Mace</li>
+                    <li>7 Wonders</li>
+                </ul>
+                <p>We also provide a signup service for any new, or surrounding shops that would like to be included to help their business run faster!</p>
+            </div>
+            <div class="col-md-6">
+                <iframe class="img-responsive" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"width="700" height="450" src="https://maps.google.com/maps?hl=en&q=Mayor Square, IFSC&ie=UTF8&t=roadmap&z=14&iwloc=B&output=embed"><div><small><a href="http://embedgooglemaps.com">embedgooglemaps.com</a></small></div><div><small><a href="http://premiumlinkgenerator.com/keep2share-cc">keep2share premium link generator</a></small></div></iframe>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Call to Action Section -->
+        <div class="well">
+            <div class="row">
+                <div class="col-md-8">
+                    <p>You can simply sign up now as a customer or vendor and begin using FoodJackal at your own leisure!</p>
+                </div>
+                <div class="col-md-4">
+                    <a class="btn btn-lg btn-primary btn-block" href="Signup/signup.php">Signup now</a>
+                </div>
+            </div>
+        </div>
