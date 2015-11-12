085d7a0caf3ab3bb61519d75f9f4d74891ba04e8
Conor Thompson <conorthompson213@gmail.com>
2015-11-12 22:40:21 +0000

    Update search_term.php

--- customer/search/search_term.php
+++ customer/search/search_term.php
@@ -4,7 +4,7 @@ if(isset($_GET['go'])){
 if(preg_match("/[A-Z | a-z]+/", $_POST['srchterm'])){
 $srchterm=$_POST['srchterm'];
 //connect to the database
-include '../classes/database/database-connect.php';
+include '../../classes/database/database-connect.php';
 $con = new Database();
 $con->connectToDatabase();
 
@@ -25,7 +25,7 @@ $title =$row['productTitle'];
 	$price=$row['productPrice'];	
 	$date = $row['productAddedDate'];
 
-	include_once("../classes/security/timeago.php"); // Include the class library
+	include_once("../../classes/security/timeago.php"); // Include the class library
         $timeAgoObject = new convertToAgo;
         $convertedTime = ($timeAgoObject -> convert_datetime($date)); // Convert Date Time
         $when = ($timeAgoObject -> makeAgo($convertedTime));
@@ -33,7 +33,7 @@ $title =$row['productTitle'];
 echo '<br><br>';
 echo '<div class="container">';
 echo '<div class="thumbnail">';
-echo '<h3><u>Product:</u> <a href="product.php?id='.$id.'">'  .$title .'</a></h3><br><p>Date: <i>' .$when. '</i></p><br>';
+echo '<h3><u>Product:</u> <a href="../product.php?id='.$id.'">'  .$title .'</a></h3><br><p>Date: <i>' .$when. '</i></p><br>';
 echo "<h4><u>Description</u>:</h4> <p><i>" .$description. "</i></p><br><h5><b>Price: €".$price."</b></h5>";
 echo "</div>";
 echo "</div>";
@@ -44,11 +44,11 @@ echo "<p>Please enter a search query</p></div></div>";
 }
 }
 else{
-	header("Location: ../");
+	header("Location: ../../");
 }
 
 }
 else{
-	header("Location: ../");
+	header("Location: ../../");
 }
 ?>
