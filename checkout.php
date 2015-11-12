67b4224d4a44016a47b1bc3b4acf0b82f44e97c7
Niall Curran <niallcurran95@gmail.com>
2015-11-12 09:46:10 +0000

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -223,8 +223,21 @@ if(isset($_COOKIE['pay'])){
                         <h4> Time of Collection: </h4>
                         <hr>
                         <h4> Price on collection: </h4>
+                        <hr> 
+                        <center><form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+				<input type="hidden" name="cmd" value="_s-xclick">
+				<input type="hidden" name="hosted_button_id" value="ZCWMNXXQY2KYW">
+				<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+				<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+			</form></center>
+                        
+                        
+                        
+                        
+                        
+                        
                     </div>
-                </br></br></br>
+                
 
                        
                     </br>
